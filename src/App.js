996c0cd5637d51ea938aac294a745f94570639e1
Ángel Becerra <angelmirocu@gmail.com>
2024-05-14 01:29:28 -0600

    test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprende reactjs
+          Aprende reactjs 
         </a>
       </header>
     </div>
