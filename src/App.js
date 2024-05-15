afc29bf98464d85362184862eae4ed6563289fb7
Ángel Becerra <angelmirocu@gmail.com>
2024-05-15 02:19:36 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./screens/home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo, este es el index de mi primer proyecto en reactjs 
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          aprende reactjs 
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <div><Home/></div>;
 }
-
-export default App;
